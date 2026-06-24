bb06086f39f30e46a0d63d15a4e2b015e2f98303
Denn Lisbon <135117785+DennLisbon@users.noreply.github.com>
2026-06-23 23:58:52 -0300

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLastWord(s) {
+        let length = 0;
+        let i = s.length - 1;
+
+        // Step 1: Skip any trailing spaces at the end of the string
+        while (i >= 0 && s[i] === ' ') {
+            i--;
+        }
+
+        // Step 2: Count the characters of the last word
+        while (i >= 0 && s[i] !== ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
