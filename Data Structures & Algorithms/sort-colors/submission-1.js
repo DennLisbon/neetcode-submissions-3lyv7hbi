ff6e8e062766a3cb17810bf23398f2bcc21becac
Denn Lisbon <135117785+DennLisbon@users.noreply.github.com>
2026-07-09 16:23:11 -0300

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let low = 0;
+        let mid = 0;
+        let high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] === 0) {
+                // Swap nums[low] and nums[mid]
+                [nums[low], nums[mid]] = [nums[mid], nums[low]];
+                low++;
+                mid++;
+            } else if (nums[mid] === 1) {
+                // 1s belong in the middle, just move forward
+                mid++;
+            } else { // nums[mid] === 2
+                // Swap nums[mid] and nums[high]
+                [nums[mid], nums[high]] = [nums[high], nums[mid]];
+                high--;
+                // Don't increment mid here because the new nums[mid] needs checking
+            }
+        }
+    }
+}
